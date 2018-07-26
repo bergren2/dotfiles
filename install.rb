6da2d58497d11865f6d6a5cd81121e9a7ca751bb
bergren2 <dan@heymrbass.com>
2018-07-26 17:22:54 -0500

    add directory copying to installation script

--- install.rb
+++ install.rb
@@ -1,26 +1,35 @@
 #!/usr/bin/env ruby
 
-require "fileutils"
+require 'fileutils'
 
-puts "Setting up vim..."
+puts 'Setting up vim...'
 
 def link_with_check(old, new)
-  FileUtils.ln_s(old, new)
-rescue
+  FileUtils.symlink(old, new)
+rescue Errno::EEXIST
   print "#{new} exists. Replace? (y/N) "
-  if $stdin.gets.chomp.downcase == "y"
+  if $stdin.gets.chomp.casecmp? 'y'
     puts "Replacing #{new}"
-    FileUtils.ln_sf(old, new)
+    FileUtils.symlink(old, new, force: true)
   else
     puts "Skipping #{new}"
   end
 end
 
-here = File.dirname(__FILE__)
+# start
 
-["vimrc", "gvimrc", "ackrc", "vrapperrc"].each do |name|
-  path = File.expand_path(name, here)
+HERE = File.dirname(__FILE__)
+DOTVIM_DIR = '~/.vim'.freeze
+
+%w[ftdetect ftplugin UltiSnips].each do |dir|
+  puts "Copying #{dir} into #{DOTVIM_DIR}..."
+  path = File.expand_path(dir, HERE)
+  FileUtils.copy_entry(path, File.expand_path(dir, DOTVIM_DIR))
+end
+
+%w[vimrc gvimrc ackrc vrapperrc].each do |name|
+  path = File.expand_path(name, HERE)
   link_with_check(path, File.expand_path("~/.#{name}"))
 end
 
-puts "done."
+puts 'done.'
